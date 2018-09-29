ab1a4d934ab1780bdd914c260c09310e62d17acb
Mikayil S <sert.mikayil@gmail.com>
2018-09-29 19:57:16 +0200

    Create Header and Navbar components

---
+++ resources/js/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Navbar from "../Navbar/Navbar"
+
+export default class Header extends React.Component {
+
+    render () {
+        return (
+            <Navbar />
+        )
+    }
+
+}
